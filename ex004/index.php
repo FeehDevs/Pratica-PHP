1d7c92cb8c1abe955251f5e58241b33ba752aec1
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-01-23 22:39:22 -0300

    Adição do ex004

---
+++ ex004/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos</title>
+</head>
+<body>
+    <h1>Tipos primitivos testes</h1>    
+    <?php 
+        // 0x = hexadecimal | 0b = Binario | 0 = Octal
+        // $num = 010;
+        // echo "O valor da variavel é $num";
+
+        // $v = ""/Felipe
+        // var_dump($v);
+
+        // $num = 3e2   // 3 * 10(2)
+
+        // $num = (integer) 3e2;
+        // echo "O valor do numero é de $num <br>";
+        // var_dump($num);
+
+        // $num = (float) 1000;
+        // echo "O valor é do tipo float né? <br>";
+        // var_dump($num)
+    ?>
+</body>
+</html>
