a554227bddd29f88ee93ec4a021c3ef5d86caefc
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-01-27 22:37:31 -0300

    Desafio Resolvido!
    Está é minha solução do desafio, com uma menção.
    
    - Não encontrei uma forma de deixar o resultado em negrito, usando apenas uma variavel

--- Desafio1/res.php
+++ Desafio1/res.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Resultado</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <section>
+        <h1>Resultado da sua pesquisa</h1>
+        <?php 
+            $num = $_GET["campo"];
+            echo "O antecessor do seu número é " . $num - 1  . "<br>";
+            echo "O número escolhido é $num <br>";
+            echo "O sucessor do seu número é " . $num + 1 . "<br>";
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a pagina anterior</a></p>
+    </section>
 </body>
 </html>
