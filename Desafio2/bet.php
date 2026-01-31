c0c1ffb9e89a1b85f1ae52ec0569b8036c31ba69
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-01-31 09:36:04 -0300

    Criação do formulario
    Resta criar a logica, primeiro pensar como colocar armazenar a escolha do html, para fazer a verificação

---
+++ Desafio2/bet.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Boa sorte!</h1>
+    </header>
+    <section>
+        <?php 
+            $sor = $_GET[""]
+        ?>
+    </section>
+</body>
+</html>
--- Desafio2/index.php
+++ Desafio2/index.php
@@ -15,9 +15,14 @@
        <p>
         Neste jogo voce irá dizer se o numero sorteado foi maior ou menor que 50
        </p>
-       <?php 
-
-       ?>
+       <form action="bet.php" method="get">
+        <label for="menor">menor</label>
+        <input type="button" value="menor" name="menor">
+        <label for="empate">empate</label>
+        <input type="button" value="empate" name="empate">
+        <label for="maior">maior</label>
+        <input type="button" value="maior" name="maior">
+       </form>
     </section>
 </body>
 </html>
