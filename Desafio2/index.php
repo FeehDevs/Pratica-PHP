e56279e9f2c73fff1152da44bdd5f0decc71b935
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-01-31 08:44:13 -0300

    Criação do arquivo

---
+++ Desafio2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Casa de aposta</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Robson BET</h1>
+    </header>
+    <section>
+       <h2>Bakara</h2>
+       <p>
+        Neste jogo voce irá dizer se o numero sorteado foi maior ou menor que 50
+       </p>
+       <?php 
+
+       ?>
+    </section>
+</body>
+</html>
