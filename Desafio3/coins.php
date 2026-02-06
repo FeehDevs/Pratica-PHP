424861e6b4dd2f3e0964577cf2ff3793cfcbc4ff
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-02-06 06:43:59 -0300

    Correção de bug
    Foi excluída a variável inexistente

--- Desafio3/coins.php
+++ Desafio3/coins.php
@@ -15,8 +15,8 @@
                 $eur = $num / 6.21;
                 $dol = number_format($dol, 2, ',', '.');
                 $eur = number_format($eur, 2, ',', '.');
-                echo "<h2>Dolar</h2><p>No dia 05/02/2026 seus R$$num equivalem a  US$$number$dol</p>";
-                echo "<h2>Euro</h2><p>No dia 05/02/2026 seus R$$num equivalem a  EUR$$number$eur</p>";
+                echo "<h2>Dolar</h2><p>No dia 05/02/2026 seus R$$num equivalem a  US$$dol</p>";
+                echo "<h2>Euro</h2><p>No dia 05/02/2026 seus R$$num equivalem a  EUR$$eur</p>";
             ?>
     </section>
 </body>
