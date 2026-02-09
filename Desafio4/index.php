a5ffb52056fd329d6ab40b1336d3691718489d65
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-02-09 17:33:30 -0300

    Utilização do method GET

--- Desafio4/index.php
+++ Desafio4/index.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conversor 2.0</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <section>
         <h1>Conversor 2.0</h1>
-        <form action="conversao.php" method="post">
+        <form action="conversao.php" method="get">
             <label for="real">Quantos R$ você quer converter</label>
             <input type="float" name="valor">
             <input type="submit" value="Converter">
         </form>
     </section>
 </body>
+
 </html>
