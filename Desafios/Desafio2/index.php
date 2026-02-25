9663fcaca1b5be448f2837f8fab741a9b96fca47
Felipe da Silva Gonçalves <felipedsg.work@gmail.com>
2026-02-25 14:41:27 -0300

    Feita identação do desafio2

--- Desafios/Desafio2/index.php
+++ Desafios/Desafio2/index.php
@@ -1,31 +1,35 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Casa de aposta</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <header>
         <h1>Robson BET</h1>
     </header>
     <section>
-       <h2>Bakara</h2>
-       <p>
-        Um número será sorteado, voce deve escolher se ele será maior, menor ou exatamanete 50.
-        <br>
-        <br>
-        <strong>Lembrando que a escala de números vai até 100</strong>
-       </p>
-       <form action="bet.php" method="get">
-        <label for="menor">< 50</label>
-        <input type="submit"  value="menor" name="menor">
-        <label for="empate">50</label>
-        <input type="submit" value="empate" name="empate">
-        <label for="maior">> 50</label>
-        <input type="submit" value="maior" name="maior">
-       </form>
+        <h2>Bakara</h2>
+        <p>
+            Um número será sorteado, voce deve escolher se ele será maior, menor ou exatamanete 50.
+            <br>
+            <br>
+            <strong>Lembrando que a escala de números vai até 100</strong>
+        </p>
+        <form action="bet.php" method="get">
+            <label for="menor">
+                < 50</label>
+                    <input type="submit" value="menor" name="menor">
+                    <label for="empate">50</label>
+                    <input type="submit" value="empate" name="empate">
+                    <label for="maior">> 50</label>
+                    <input type="submit" value="maior" name="maior">
+        </form>
     </section>
 </body>
+
 </html>
